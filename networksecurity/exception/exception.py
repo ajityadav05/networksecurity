b5514262d1f41d300a91f11f9d52c98a1f71e23f
Ajit Yadav <yadavajitdada@gmail.com>
2026-05-13 14:52:46 +0530

    Exception Handling and Logging

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,21 @@
+import sys
+from networksecurity.logging.logger import logging
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.filename = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occurred in script [{self.filename}] at line number [{self.lineno}] error message [{self.error_message}]"
+
+# if __name__ == "__main__":
+#     try:
+#         logging.info("Entered the try block.")
+#         a = 1/0
+#     except Exception as e:
+#         raise NetworkSecurityException(e,sys)
+        
